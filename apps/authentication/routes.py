7d3284d70eebdecea447a51480003cfeb015e082
Kali Linux <kali@ip-172-31-38-154.eu-west-3.compute.internal>
2023-02-09 03:57:55 +0200

    logo templates

--- apps/authentication/routes.py
+++ apps/authentication/routes.py
@@ -3,10 +3,7 @@
 Copyright (c) 2019 - present AppSeed.us
 """
 from flask import render_template, redirect, url_for
-from flask_login import (
-    logout_user
-)
-
+from flask_login import logout_user
 from apps import login_manager
 from apps.authentication import blueprint
 from apps.authentication.models import Users
@@ -16,32 +13,23 @@ from apps.authentication.models import Users
 def route_default():
     return redirect(url_for('home.html'))
 
-# @blueprint.route('/index')
-# def route_index():
-#     return redirect(url_for('home/index.html'))
-
 @blueprint.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('authentication_blueprint.login'))
 
-
 # Errors
-
 @login_manager.unauthorized_handler
 def unauthorized_handler():
     return render_template('home/page-403.html'), 403
-
 
 @blueprint.errorhandler(403)
 def access_forbidden(error):
     return render_template('home/page-403.html'), 403
 
-
 @blueprint.errorhandler(404)
 def not_found_error(error):
     return render_template('home/page-404.html'), 404
-
 
 @blueprint.errorhandler(500)
 def internal_error(error):
--- apps/home/routes.py
+++ apps/home/routes.py
@@ -6,28 +6,12 @@ from apps.home import blueprint
 from flask import render_template, request, Flask
 from jinja2 import TemplateNotFound
 
+
 app = Flask(__name__)
 
 @app.route("/")
-@app.route("/home")
 def home():
     return render_template("home.html")
-
-@app.route("/hebrew")
-def hebrew():
-    return render_template("hebrew.html")
-
-@app.route("/russian")
-def russian():
-    return render_template("russian.html")
-
-@app.route("/english")
-def english():
-    return render_template("english.html")
-
-# @blueprint.route('/index')
-# def index():
-#     return render_template('home/index.html', segment='index')
 
 
 @blueprint.route('/<template>')
@@ -45,16 +29,16 @@ def route_template(template):
         return render_template('home/page-404.html'), 404
 
     except:
-        return render_template('home/page-500.html'), 500
+        return render_template('home/index.html'), 500
 
 # Helper - Extract current page name from request
-# def get_segment(request):
+def get_segment(request):
 
-#     try:
-#         segment = request.path.split('/')[-1]
-#         if segment == '':
-#             segment = 'index'
-#         return segment
+    try:
+        segment = request.path.split('/')[-1]
+        if segment == '':
+            segment = 'index'
+        return segment
 
-#     except:
-#         return None
+    except:
+        return None
